b1960f443826fccf01a0f6b04e6daa1a2713204d
VisualStudy <sparksoldier@outlook.com>
2024-05-06 21:11:44 +0900

    Create Person Table

---
+++ SQL/SQL/dbo/Tables/Person.sql
@@ -0,0 +1,10 @@
+﻿-- Create Person table
+CREATE TABLE Person
+(
+    PersonId INT IDENTITY(1, 1) PRIMARY KEY,
+    FirstName NVARCHAR(128) NOT NULL,
+    MiddleInitial NVARCHAR(10) NULL,
+    LastName NVARCHAR(128) NOT NULL,
+    DateOfBirth DATE NOT NULL DEFAULT(GetDate())
+)
+Go
