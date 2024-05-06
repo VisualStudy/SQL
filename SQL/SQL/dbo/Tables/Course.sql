ce1151de5dde47528b65d752e246e56f302ec5d0
VisualStudy <sparksoldier@outlook.com>
2024-05-06 21:27:08 +0900

    bug fix

--- SQL/SQL/dbo/Tables/Course.sql
+++ SQL/SQL/dbo/Tables/Course.sql
@@ -1,7 +1,7 @@
 ﻿-- Create Course table
 CREATE TABLE Course
 (
-    CouresId INT IDENTITY(1, 1) PRIMARY KEY,
+    CourseId INT IDENTITY(1, 1) PRIMARY KEY,
     Name NVARCHAR(50) NOT NULL,
     Tecaher NVARCHAR(256) NOT NULL
 )
